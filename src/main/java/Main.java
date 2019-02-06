6a2031e8f103bfa5817c90e024aa37f19e76471a
lucefer13 <lucefer13+github@gmail.com>
2019-02-06 13:48:33 +0600

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,83 @@
+
+import jssc.*;
+
+import java.io.IOException;
+public class Main {
+
+    private static SerialPort serialPort;
+
+    static String portFinder() {
+        String stat ="failed - Port no found";
+        SerialPortList serialPortList = new SerialPortList();
+        for (String portlist :serialPortList.getPortNames()){
+            serialPort = new SerialPort(portlist);
+            String test;
+            try {
+                serialPort.openPort();
+                serialPort.setParams(SerialPort.BAUDRATE_9600,
+                        SerialPort.DATABITS_8,
+                        SerialPort.STOPBITS_1,
+                        SerialPort.PARITY_NONE);
+                serialPort.setFlowControlMode(SerialPort.FLOWCONTROL_RTSCTS_IN |
+                        SerialPort.FLOWCONTROL_RTSCTS_OUT);
+                test = serialPort.readString();
+                int i = 0;
+                while ((test == null) & (i<5000000)) {
+                    test = serialPort.readString();
+                    i++;
+                }
+                serialPort.closePort();
+                if ((test != null)&&(test.contains("accelstart"))) {
+                    stat = portlist;
+                    break;
+                }
+            }
+            catch (SerialPortException ex) {
+                stat ="failed " + ex.toString();
+            }
+        }
+        return stat;
+    }
+
+    public static void main(String[] args) throws IOException {
+        //Todo сделать проверку на "Failed"
+        serialPort = new SerialPort(portFinder());
+        //todo допилить трап сендер
+        TrapSender trap = new TrapSender();
+        trap.sendTrap("192.168.111.103"); //todo получить аргументы параметров запуска IP
+        try {
+            serialPort.openPort();
+            //Выставляем параметры
+            serialPort.setParams(SerialPort.BAUDRATE_9600,
+                    SerialPort.DATABITS_8,
+                    SerialPort.STOPBITS_1,
+                    SerialPort.PARITY_NONE);
+            //Включаем аппаратное управление потоком
+            serialPort.setFlowControlMode(SerialPort.FLOWCONTROL_RTSCTS_IN |
+                    SerialPort.FLOWCONTROL_RTSCTS_OUT);
+            //Устанавливаем ивент лисенер и маску
+            serialPort.addEventListener(new PortReader(), SerialPort.MASK_RXCHAR);
+        }
+        catch (SerialPortException ex) {
+            System.out.println("Failed " + ex);
+        }
+    }
+    private static class PortReader implements SerialPortEventListener {
+        public void serialEvent(SerialPortEvent event) {
+            if(event.isRXCHAR() && event.getEventValue() > 0){
+                try {
+                    //Получаем ответ от устройства, обрабатываем данные и т.д.
+                    String data = serialPort.readString(event.getEventValue());
+                    //И снова отправляем запрос
+                    System.out.println(data);
+                    //    todo распарсить строку на x y z
+                    //    todo сделать проверку на превышения порога получить из параметров запуска
+                    //serialPort.writeString("Get data");
+                }
+                catch (SerialPortException ex) {
+                    System.out.println(ex);
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/TrapSender.java
@@ -0,0 +1,56 @@
+
+
+import org.snmp4j.CommunityTarget;
+import org.snmp4j.PDU;
+import org.snmp4j.Snmp;
+import org.snmp4j.TransportMapping;
+import org.snmp4j.mp.SnmpConstants;
+import org.snmp4j.smi.*;
+import org.snmp4j.transport.DefaultUdpTransportMapping;
+
+import java.io.IOException;
+import java.util.Date;
+
+public class TrapSender {
+    public static final String community = "public";
+    // Sending Trap for sysLocation of RFC1213
+    public static final String Oid = ".1.3.6.1.2.1.1.8";
+    //IP of Local Host
+    public static String ipAddress = "";
+    //Ideally Port 162 should be used to send receive Trap, any other available Port can be used
+    public static final int port = 162;
+
+    public void sendTrap(String str) throws IOException {
+        /* Create Transport Mapping */
+        ipAddress = str;
+        TransportMapping transport = new DefaultUdpTransportMapping();
+        transport.listen();
+        // Create Target
+        CommunityTarget cTarget = new CommunityTarget();
+        cTarget.setCommunity(new OctetString(community));
+        cTarget.setVersion(SnmpConstants.version2c);
+        cTarget.setAddress(new UdpAddress(ipAddress + "/" + port));
+        cTarget.setRetries(2);
+        cTarget.setTimeout(5000);
+        // Create PDU for V2
+        PDU pdu = new PDU();
+        // need to specify the system up time
+        pdu.add(new VariableBinding(SnmpConstants.sysUpTime,
+                new OctetString(new Date().toString())));
+        pdu.add(new VariableBinding(SnmpConstants.snmpTrapOID, new OID(
+                Oid)));
+        pdu.add(new VariableBinding(SnmpConstants.snmpTrapAddress,
+                new IpAddress(ipAddress)));
+        pdu.add(new VariableBinding(new OID(Oid),new OctetString(
+                str)));
+        pdu.setType(PDU.NOTIFICATION);
+        // Send the PDU
+        Snmp snmp = new Snmp(transport);
+        //   System.out.println("Sending V2 Trap... Check Wheather NMS is Listening or not? ");
+        snmp.send(pdu, cTarget);
+        snmp.close();
+    }
+/*         catch (Exception e) {
+                e.printStackTrace();
+        }*/
+}
