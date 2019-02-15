e34ead53101ac4883a93894da595d0cc8dc76a8f
lucefer13 <lucefer13+github@gmail.com>
2019-02-15 14:55:34 +0600

    +Добавлена дата событий +Поиск по порту ограничен 5 секундами + Консольное приложение готово!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,11 @@
 import jssc.*;
 import java.io.IOException;
+import java.util.Date;
 import java.util.StringTokenizer;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
+    //private static Date date;
     private  static int X,Y,Z;
     private static SerialPort serialPort;
     private static void parcer(String str1){
@@ -22,10 +25,11 @@ public class Main {
     }
     private static String portFinder()   {
         String stat ="failed - Port no found";
+        System.out.println(new Date().toString() + " Searching...");
         for (String portlist : SerialPortList.getPortNames()){
             serialPort = new SerialPort(portlist);
             String test;
-            System.out.println(portlist + " search...");
+            //System.out.println(portlist + " search...");
             try {
                 serialPort.openPort();
                 serialPort.setParams(SerialPort.BAUDRATE_9600,
@@ -34,24 +38,24 @@ public class Main {
                         SerialPort.PARITY_NONE);
                 serialPort.setFlowControlMode(SerialPort.FLOWCONTROL_RTSCTS_IN |
                         SerialPort.FLOWCONTROL_RTSCTS_OUT);
+                TimeUnit.SECONDS.sleep(5);
                 test = serialPort.readString();
-                int i = 0;
-                while ((test == null) & (i<5000000)) { //Задержка - сколько?
-                    test = serialPort.readString();
-                    i++;
-                }
                 serialPort.closePort();
-                if ((test != null)&&(test.contains("accelstart"))) {
-                    stat = portlist;
-                    System.out.println(portlist + " successfully...");
-                    break;
-                }
-                System.out.println(portlist + " not found...");
+               // System.out.println(test);
+                    if (test != null)
+                        if (test.contains("accelstart")){
+                            stat = portlist;
+                            System.out.println(new Date().toString() + " " + portlist + " successfully...");
+                            break;
+                        }
+                //System.out.println(portlist + " not found...");
 
             }
             catch (SerialPortException ex) {
-                System.out.println(portlist + " not found...");
+                System.out.println(new Date().toString() + " " + portlist + " not found...");
                 stat ="failed " + ex.toString();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
         return stat;
@@ -62,24 +66,24 @@ public class Main {
     private static String PORT = "";
 
     public static void main(String[] args) throws IOException {
-        for (String stt : args) {
+       /* for (String stt : args) {
             System.out.println(stt);
-        }
-        if (args.length == 0) System.out.println("incorrect start key...");
+        }*/
+        if (args.length == 0) System.out.println(new Date().toString() + " incorrect start key...");
 
-        if (args.length == 1) {
-            if (args[0].equals("findCom")) System.out.println("Accel port is " + portFinder());
-            else System.out.println("incorrect start key...");
+        if (args.length == 1) { //findCom
+            if (args[0].equals("findCom")) System.out.println(new Date().toString() + " Accel port is " + portFinder());
+            else System.out.println(new Date().toString() + " incorrect start key...");
         }
 
-        if (args.length == 2) {
+        if (args.length == 2) { //IP SENSYTIVE
             IPADRES = args[0];
             SENSYTY = Integer.parseInt(args[1]);
             PORT = portFinder();
             ComOpener();
         }
 
-        if (args.length == 3){
+        if (args.length == 3){ //SNMP TEXT
             if (args[0].equals("snmp")) {
                 IPADRES = args[1];
                 try {
@@ -88,7 +92,7 @@ public class Main {
                     System.out.println("incorrect IP :" + e);
                 }
             }
-            else {
+            else { //IP SENSYTIVE COM
                 StringTokenizer st = new StringTokenizer(args[0], ".");
                 if (st.countTokens() == 4) {
                     IPADRES = args[0];
@@ -101,13 +105,19 @@ public class Main {
         }
 
         if (args.length > 3) {
-            System.out.println("incorrect start key...");
+            System.out.println(new Date().toString() + " incorrect start key...");
         }
     }
 
     private static void ComOpener() {
-        if (PORT.contains("failed")) System.out.println("AccelSensor in not connected...");
+        if (PORT.contains("failed")) System.out.println( new Date().toString() + " AccelSensor in not connected...");
         else {
+            System.out.println();
+            System.out.println("\t" +"Sens" + "\t\t" + SENSYTY);
+            System.out.println("\t" +"Port" + "\t\t" + PORT);
+            System.out.println("\t" +"Snmp IP" +  "\t\t" + IPADRES);
+            System.out.println();
+            System.out.println( new Date().toString() + " Accel is start...");
             serialPort = new SerialPort(PORT);
             try {
                 serialPort.openPort();
@@ -122,7 +132,8 @@ public class Main {
                 //Устанавливаем ивент лисенер и маску
                 serialPort.addEventListener(new PortReader(), SerialPort.MASK_RXCHAR);
             } catch (SerialPortException ex) {
-                System.out.println("failed " + ex);
+                //System.out.println("failed " + ex);
+                System.out.println(new Date().toString() + " Accel on " + PORT + " not found");
             }
         }
     }
@@ -133,14 +144,15 @@ public class Main {
                     //Получаем ответ от устройства, обрабатываем данные и т.д.
                     parcer(serialPort.readString(event.getEventValue()));
                     if ((X>SENSYTY)||(Y>SENSYTY)||(Z>SENSYTY)) {
+                        System.out.println(new Date().toString() + " Move detect");
                         try {
                             TRAP.sendTrap(IPADRES,"Move detect");
                         } catch (IOException e) {
                            // e.printStackTrace();
-                            System.out.println(e);
+                            //System.out.println(e);
                         }
                     }
-                    // System.out.println(""+ X + Y + Z);
+                   // System.out.println(""+ X + Y + Z);
                 }
                 catch (SerialPortException ex) {
                     //System.out.println(ex);
